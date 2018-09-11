9401a6b46afbc2a416de0a11dba5370670132660
Michael Muniz <michael.muniz94@gmail.com>
2018-09-11 04:40:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ function destructivelyDeleteFromObjectByKey(object, key) {
    var recipes = { prop: 1 }
    var newRecipes = Object.assign(recipes, 'prop')
    delete newRecipes['prop']
-   delete recipes
+   delete recipes['prop']
  }
