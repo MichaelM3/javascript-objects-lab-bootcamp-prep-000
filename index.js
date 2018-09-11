71bee3a7f963e1c7d709bdc20842cb0ba8ef5a30
Michael Muniz <michael.muniz94@gmail.com>
2018-09-11 03:08:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   var recipes = { prop: 1 }
-  recipes['prop2'] = 2
-  return recipes
+  return Object.assign(recipes, {prop2: 2})
 }
