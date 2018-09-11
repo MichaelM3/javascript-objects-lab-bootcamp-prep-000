0c7b02bcc047a4bd741a610466ff498bc6aac7d1
Michael Muniz <michael.muniz94@gmail.com>
2018-09-11 04:51:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function deleteFromObjectByKey(object, key) {
  }
  
 function destructivelyDeleteFromObjectByKey(object, key) {
-   var recipes = { prop: 1 }
-   var newRecipes = 
+   delete object[key]
+   return object
    
 }
