9603774389c08f405b6aad0dc039d7fd3914f341
Michael Muniz <michael.muniz94@gmail.com>
2018-09-11 02:28:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   var recipes = { prop: 1 }
-  return object.assign({ prop2: 2 })
+  return object.assign(recipes, { prop2: 2 })
 }
