7ab9029d3247adc4f23f0ee97088d6b385fdd7f8
Michael Muniz <michael.muniz94@gmail.com>
2018-09-11 03:20:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   var recipes = { prop: 1 }
   recipes['prop2'] = 2
-  
+  return recipes
 }
